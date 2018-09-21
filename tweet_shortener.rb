d1a56a05ea895d6158be1bb2e187e206fa20cf62
Harry Bloch <harryfbloch@gmail.com>
2018-09-21 17:25:52 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def dictionary
+  dict = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "@"
+  }
+end
+
+def word_substituter(tweet_one)
+  dict = dictionary()
+  tempArray = []
+  tweetArray = tweet_one.split(" ")
+  tweetArray.each {|word| 
+    dictionary.each {|key, value|
+      if word == value
+       word = key
+      end
+       tempArray << word
+    
+    }
+    returnTweet = tempArray.join(" ")
+  }
+end
