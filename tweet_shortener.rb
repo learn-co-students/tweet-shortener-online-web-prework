2e7570dd67f354342b8680578602bc11f3b06cb3
Chris Philbin <chris@chrisphilbin.net>
2018-11-20 03:24:07 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def dictionary
+  short = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  array = tweet.split(" ")
+
+end
