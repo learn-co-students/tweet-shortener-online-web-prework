c987dcef2edc587a7fa4b5978e11254cfb69a0c1
Robert McCary <robert.mccary@yahoo.com>
2018-12-07 18:16:04 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,23 +1,39 @@
-
-
+def dictionary
+  dictionary = {"Hey"=> "hi", "to"=> "2", "two"=> "2",
+    "too"=> "2", "for"=> "4", "four"=> "4", "be"=> "b",
+    "you"=> "u", "at"=> "@", "and"=> "&"}
+end
 
 
 def word_substituter(tweet)
-  answer = ""
-  dictionary = {"Hey"=> "hi", "to"=> "2", "two"=> "2", "too"=> "2", "for"=> "4", "four"=> "4", "be"=> "b", "you"=> "u", "at"=> "@", "and"=> "&"}
-tweet_array = tweet.split(" ")
-# tweet_array << tweet.to_a
-  tweet_array.map { |word|
-    dictionary.each do |k,v|
-      if word == k
-        puts "k = #{k}"
-        puts "word = #{word}"
-        v
-        break
-      end
-    end
-}
-  tweet_array.each{ |word| answer << " #{word}"}
-  answer[1..-1]
+tweet.split.collect do |word|
+  if dictionary.keys.include?(word.downcase)
+    word = dictionary[word.downcase]
+  else
+    word
+  end
+end.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+  tweet.each do |tweet|
+  puts  word_substituter(tweet)
   end
 end
+
+def selective_tweet_shortener(tweet)
+    if tweet.length > 140
+       word_substituter(tweet)
+    else
+      tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+     word_substituter(tweet)
+     tweet[0..139]
+   else
+     tweet
+   end
+ end
