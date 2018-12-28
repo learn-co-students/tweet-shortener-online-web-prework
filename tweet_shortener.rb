e6505c9bbba830f27a52961ff79fdda861756e55
Sim Greenbaum <gbs4ever2@gmail.com>
2018-12-28 05:11:35 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+require 'pry'# Write your code here.
+def dictionary #(hashs)
+hashs = { "hello" => "hi","to two too" => "2","for four" => "4",
+"be" => "b",
+"you"=> "u",
+"at" => "@",
+"and" => "&"
+}
+end
+
+def  word_substituter (string)
+  words=dictionary.keys.join("  ")
+binding.pry
+
+  string.split.each do |change |
+  change.include?(words)
+#binding.pry
+
+end
+end
