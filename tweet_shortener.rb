ea25888bea13883754343050df04b144f6d829d7
Sim Greenbaum <gbs4ever2@gmail.com>
2018-12-30 05:16:06 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,7 +4,10 @@ def dictionary #(hashs)
 hashs = { "hello" => "hi","to"=> "2",
 "two"=> "2",
 "too" => "2",
-"for four" => "4",
+"for" =>"4",
+"For" =>"4",
+
+"four" => "4",
 "be" => "b",
 "you"=> "u",
 "at" => "@",
@@ -13,14 +16,37 @@ hashs = { "hello" => "hi","to"=> "2",
 end
 
 def  word_substituter (string)
-  words=dictionary.keys
-arraytw =string.split(" ")
-
-#if words.include?(string) == true
-arraytw.each do  |word|
-   words.include?(word)
-  word.gsub(dictionary)
-# delete_if  iclude?() .select
+  words = dictionary.keys
+#arraytw =string.split(" ")
+string.split.collect do |word|
+  if words.include?(word)
+    word = dictionary[word]
+  else
+    word
+  end
+#arraytw =arraytw.join()
+#return  arraytw
+end.join(" ")
+end
+ def bulk_tweet_shortener(array)
+array.each  do  |tweets|
+puts word_substituter(tweets)
+end
 
 end
+
+
+def selective_tweet_shortener(strings)
+  if strings.length  >  140
+     word_substituter(strings)
+else
+   strings
+end
+end
+def shortened_tweet_truncator(longtweet)
+  if longtweet.length > 140
+ newstring   = longtweet[0..139]
+else
+  longtweet
+  end
 end
