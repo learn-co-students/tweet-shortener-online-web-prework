7eda2916b59c4ce58840f08916c25c23df9bddbe
michael hussain <smhussain_1@yahoo.com>
2019-01-06 16:05:15 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+require "pry"
+
+  #create a hash, where long are keys and new words are values.
+
+def dictionary
+  dictionary = {
+    "hello" => "hi", 
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end 
+  
