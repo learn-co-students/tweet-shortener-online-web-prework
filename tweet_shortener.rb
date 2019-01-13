7fae7bdfacc7dc9923a3bddb309be80b6bfd48e4
A Glazer <avigayilglazer@gmail.com>
2019-01-13 20:25:53 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -34,3 +34,28 @@ tweets_array.map do |tweet|
   puts word_substituter(tweet)
 end
 end
+
+
+
+def selective_tweet_shortener(tweet_string)
+if tweet_string.length > 140
+  word_substituter(tweet_string)
+else
+  tweet_string
+end
+
+
+def shortened_tweet_truncator(tweet_string)
+  if word_substituter(tweet_string).length > 140
+    140_char = tweet_string[0..139]
+    "#{140_char}..."
+
+    binding.pry
+  else
+    tweet_string
+
+  end
+end
+
+
+end
