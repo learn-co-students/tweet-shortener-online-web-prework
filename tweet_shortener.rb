35611c9f8616c37abae555bac1f751fde142477d
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-27 06:53:35 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,49 @@
 # Write your code here.
+
+def dictionary
+  words = {
+     "hello" => "hi",
+     "to" => "2",
+     "two" => "2",
+     "too" => "2",
+     "for" => "4",
+     "four" => "4",
+     "be" => "b",
+     "you" => "u",
+     "at" => "@",
+     "and" => "&"
+   }
+ end
+
+
+def word_substituter(tweet)
+  tweet.split(" ").map do |word|
+    dictionary.each do |key, value|
+      word = value if word.downcase == key
+    end
+    word
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+  tweet.each do |word|
+    puts word_substituter(word)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.chars.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).chars.length > 140
+    array = word_substituter(tweet).chars[0..136].push("...").join("")
+  else
+    word_substituter(tweet)
+  end
+end
