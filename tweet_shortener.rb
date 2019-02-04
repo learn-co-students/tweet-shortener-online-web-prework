519fd3c76ed1bd3471b2b6c49deb24ca575b1cdb
Christoph Gruener <christophgruener@me.com>
2019-02-04 23:55:42 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+require 'pry'
+def dictionary
+ 
+  dictionary = {  "hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u",  "at" => "@", "and" => "&"}
+
+end
+
+def word_substituter(tweets)
+  tweets_ary = tweets.split(" ")
+  dictionary
+  tweets_ary.each.with_index do |word, index|
+    if dictionary.keys.join(" ").include?(word)
+      tweets_ary[index] = dictionary[word]
+    end
+  end
+  tweets_ary.join(" ")
+end
