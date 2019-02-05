e688c4ded15b82b2c4fada53f961bf51adf2ca89
Mark Kanko <mkanko66@gmail.com>
2019-02-05 16:25:43 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,40 @@
-# Write your code here.
+require 'pry'
+
+def dictionary 
+  short_word_list = {
+    "hello" => 'hi', 
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four"=> '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => '@',
+    "and" => '&'
+  }
+end 
+
+def word_substituter(tweet)
+  
+  compare_tweet = tweet.split
+  
+    compare_tweet.each do |word|
+      
+      if dictionary.keys.include?(word) == compare_tweet(word.downcase) 
+        
+  binding.pry
+
+        
+        
+      end 
+      
+    end 
+    
+end 
+  
+  
+  
+    
+    
+  
