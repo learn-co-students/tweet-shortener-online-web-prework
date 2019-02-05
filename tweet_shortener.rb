0b6ba5e1d14642be02b9a643e071c82500f4778b
Christoph Gruener <christophgruener@me.com>
2019-02-05 00:25:47 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -30,3 +30,15 @@ def selective_tweet_shortener(tweet)
   tweet
   end
 end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0..136] + "..."
+  else
+    word_substituter(tweet)
+  end
+end
+  
+  
+  
+  
