8414d90494bd36d623cd2a9f5c9cb45cb97f87f8
Mark Kanko <mkanko66@gmail.com>
2019-02-06 00:30:16 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,6 +39,16 @@ def selective_tweet_shortener(tweet)
     tweet  
   end    
 end  
+
+
+def shortened_tweet_truncator(tweet)
+  
+  if word_substituter(tweet).length > 140 
+    word_substituter(tweet).chars[0..136].push("...").join("")  
+  else 
+    word_substituter(tweet)
+  end 
+end  
     
     
   
