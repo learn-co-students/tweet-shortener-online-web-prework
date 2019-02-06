12f6da80b85764f45ab235d7ff81fc0eadffe5f5
Mark Kanko <mkanko66@gmail.com>
2019-02-06 00:39:15 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,11 +33,13 @@ end
 
 
 def selective_tweet_shortener(tweet)
-  if tweet.length > 140      #? word_substituter(tweet) : tweet
-    word_substituter(tweet)
-  else 
-    tweet  
-  end    
+   tweet.length > 140 ? word_substituter(tweet) : tweet
+   
+#  if tweet.length > 140
+#    word_substituter(tweet)
+#  else 
+#    tweet  
+#  end    
 end  
 
 
