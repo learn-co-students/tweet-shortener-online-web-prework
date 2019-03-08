34647eb1681bb24b001c6ed8af64247496d1a0f9
AJ Guiles <ashiguiles@gmail.com>
2019-03-08 01:24:08 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,35 +15,35 @@ def dictionary
 end
 
 def word_substituter(tweet)
-  tweet.split(" ").collect do |word|
-      if dictionary.keys.include?(word.downcase)
-        word = dictionary[word.downcase]
+  tweet.split(" ").collect do |key| #splite tweet into array of strings and collect that array and yield to block
+      if dictionary.keys.include?(key.downcase) # if word in array is included in dictionary then return true  
+        key = dictionary[key.downcase] # for words in array that return true, replace with value of included dictionary key  
       else
-        word
+        key # return new array 
     end
-  end.join(" ")
+  end.join(" ") #convert returned array into string with a space between each word 
 end
 
-def bulk_tweet_shortener(tweet_array)
-  tweets.each do |tweet|
-    puts word_substituter(tweet)
+def bulk_tweet_shortener(tweet_array) 
+  tweets.each do |tweet|    # iterate through each tweet and yield to block
+    puts word_substituter(tweet)  # apply the above word_substituter method to the yeilded tweet
   end
 end
  
- def selective_tweet_shortener(tweet)
-  if tweet.length > 140
-    word_substituter(tweet)
+ def selective_tweet_shortener(tweet) 
+  if tweet.length > 140     # if the lenght of the tweet is less than 140
+    word_substituter(tweet) # then apply the word_substituter method 
   else
-    tweet
+    tweet                 
   end
 end
 
-def shortened_tweet_truncator(tweet)
-  short = word_substituter(tweet)
-  if short.length > 140
-    short[0...140]
+def shortened_tweet_truncator(tweet) 
+  short = word_substituter(tweet) #  "short" is additional modifications to original word_substituter method
+  if short.length > 140          # "short" => if word_substituter return tweet that is greater than 140 characters
+    short[0...140]              # then shorten tweet by limiting the tweet's index to rannge between 0 and 140
   else
-    short
+    short                     
   end
 end
  
