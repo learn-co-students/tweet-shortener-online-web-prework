778de11027b8fdabde85b59f63e9795cfff6f0df
David Thayer <dthayer16@gmail.com>
2019-03-09 00:25:00 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+
+def dictionary
+  {"hello" => "hi",
+    "to" => "2", "two" => "2", "too" => "2",
+    "for" => "4", "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&" }
+end
+
+def word_substituter(string)
+  words = string.split
+  keys = dictionary.keys
+
+  words.each do |word|
+    if word == keys
+      words << keys
+    end
+  end
+  words.join(" ")
+end
+
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener
+end
+end
