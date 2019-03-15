271287ce5a0f69f715a8b85e17d5bb7e39347f5d
active-object-6703 <nerajno@gmail.com>
2019-03-15 04:50:11 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+dictionary = {
+  "hello" => 'hi',
+  "be" => 'b',
+  "you" => 'u',
+  "at" => '@',
+  "and" => '&',
+  "to" =>'2',
+  "two" => '2',
+  "too" => '2',
+  "for" => '4',
+  "four" => '4'
+}
+
+def word_substituter(tweet, dictionary)
+  wrking_grnd = tweet.split(" ")
+  rtnd_wrds = ''
+  wrking_grnd.each do |wrd|
+    #notes
+    #iterate through the code
+    #push the word to the new array if its not in the dic
+    #otherwise, make the substitution
+    if dictionary.has_key?wrd
+      wrd = dictionary[wrd]
+     rtnd_wrds << wrd+" "
+    else
+     rtnd_wrds << wrd
+    end
+  end
+   rtnd_wrds
+  end #end the collection of the new array
