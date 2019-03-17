ccff6797db1519710e66decae2e0ac146f9a1c7f
active-object-6703 <nerajno@gmail.com>
2019-03-17 22:01:29 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,31 +1,34 @@
 # Write your code here.
-dictionary = {
-  "hello" => 'hi',
-  "be" => 'b',
-  "you" => 'u',
-  "at" => '@',
-  "and" => '&',
-  "to" =>'2',
-  "two" => '2',
-  "too" => '2',
-  "for" => '4',
-  "four" => '4'
-}
-
-def word_substituter(tweet, dictionary)
+# Word Substituter
+def word_substituter(tweet)
+  dictionary = {
+    "hello" => 'hi',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => '@',
+    "and" => '&',
+    "to" =>'2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4'
+  }
   wrking_grnd = tweet.split(" ")
-  rtnd_wrds = ''
-  wrking_grnd.each do |wrd|
-    #notes
-    #iterate through the code
-    #push the word to the new array if its not in the dic
-    #otherwise, make the substitution
-    if dictionary.has_key?wrd
-      wrd = dictionary[wrd]
-     rtnd_wrds << wrd+" "
+  result = []
+  wrking_grnd.collect do |wrd|
+    if dictionary.has_key?(wrd.downcase)
+      nw_wrd = dictionary[wrd]
+      result +=[nw_wrd]
     else
-     rtnd_wrds << wrd
+      result +=[wrd]
     end
   end
-   rtnd_wrds
-  end #end the collection of the new array
+   result.join(" ")
+  end
+
+
+def bulk_tweet_shortener(array)
+  array.each do |each_tweet|
+    puts word_substituter(each_tweet)
+  end
+end
