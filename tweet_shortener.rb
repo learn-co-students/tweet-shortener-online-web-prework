6b80d01504c0ddb3506c90ef387da93b862d08dc
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-23 04:37:20 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,36 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+    }
+end  
+  
+def word_substituter(tweets)
+  tweets.split.map do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join
+end 
+
+def bulk_tweet_shortener
+  
+end
+
+def selective_tweet_shortener
+  
+end
+
+def shortened_tweet_truncator
+  
+end  
