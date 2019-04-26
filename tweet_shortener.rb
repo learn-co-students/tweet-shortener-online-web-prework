78c8eeceaa9b0b441419a0110da2ae02d7429de9
Denali Balser <denali.balser167@gmail.com>
2019-04-26 19:18:28 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -28,3 +28,19 @@ def bulk_tweet_shortener(tweet)
     puts word_substituter(phrase)
   end
 end
+
+def selective_tweet_shortener(tweet)
+  if tweet.chars.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).chars.length > 140
+    truncated = word_substituter(tweet).chars[0..136].push("...").join("")
+  else
+    word_substituter(tweet)
+  end
+end 
