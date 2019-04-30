34b959a0e0be99a211b54f454dae57fad9ef66e3
Jamie Vullo <jvullo@protonmail.com>
2019-04-30 13:44:46 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,55 @@
-# Write your code here.
+require "pry"
+
+def dictionary 
+  shorten = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+    }
+end
+
+def word_substituter(tweet_one)
+  tweet_one.split(" ").map do |word|
+    if dictionary.keys.include?(word.downcase) 
+      word = dictionary[word.downcase]  
+    else
+      word 
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+  tweet.map do |message|
+    puts word_substituter(message)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+ # binding.pry
+  
+    if tweet.length > 140
+      word_substituter(tweet)
+    elsif tweet.length <= 140
+      tweet
+    
+  end 
+end   
+
+def shortened_tweet_truncator(tweet)
+  #binding.pry
+  
+    if tweet.length > 140
+      word_substituter(tweet)[0..136] + "..."
+    else 
+      tweet
+    
+  end
+end  
+
