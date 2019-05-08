348ef78ddafa8f897c826c214bceaef49df39e55
Travis Exline <t.exline22@gmail.com>
2019-05-08 15:16:16 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def dictionary
+dictionary = {
+  "hello" => "hi",
+  "too" => "2",
+  "to" => "2",
+  "two" =>"2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end
+
+def word_substituter(tweet)
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweets_array)
+  tweets_array.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
