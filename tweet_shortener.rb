c6fe5c0c436c7ab8f1ebee3ef27541ba2b3b30ca
Joshua Crum <joshua.crum@gmail.com>
2019-05-09 19:34:43 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,5 @@
 def dictionary
-  dict = {
+  translate = {
     "hello" => 'hi',
     "to"=> "2",
     "too"=> "2",
@@ -17,12 +17,36 @@ end
 
 def word_substituter(tweet)
   words = tweet.split(" ")
+  translate = dictionary
   words.map do |word|
     dictionary.map do |k, v|
-      if word = k
-        word.replace(value)
+      if word == k
+        word.replace(v)
       end
     end
   end
   words.join(" ")
+end
+
+def bulk_tweet_shortener(tweets_array)
+  tweets_array.collect do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+
+def selective_tweet_shortener(tweet)
+  if tweet.chars.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).chars.length > 140
+    truncated = word_substituter(tweet).chars[0..136].push("...").join("")
+  else
+    word_substituter(tweet)
+  end
 end
