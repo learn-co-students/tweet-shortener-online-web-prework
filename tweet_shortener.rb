e9febf23ee046a135260d1e18083c3ac23c22dd8
Joshua Crum <joshua.crum@gmail.com>
2019-05-09 19:24:02 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def dictionary
+  dict = {
+    "hello" => 'hi',
+    "to"=> "2",
+    "too"=> "2",
+    "two" => "2",
+    "for"=>"4",
+    "four" => "4",
+    "For"=>"4",
+    "Four" => "4",
+    'be' => "b",
+    'you'=> "u",
+    "at" =>"@",
+    "and" => "&",
+    }
+end
+
+def word_substituter(tweet)
+  words = tweet.split(" ")
+  words.map do |word|
+    dictionary.map do |k, v|
+      if word = k
+        word.replace(value)
+      end
+    end
+  end
+  words.join(" ")
+end
