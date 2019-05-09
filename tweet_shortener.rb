798862da2b86bddf6a1335f23f07dc37f20881ec
Angela Lo <angelawenlo@gmail.com>
2019-05-09 15:42:40 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,49 @@
 # Write your code here.
+def dictionary
+  convert = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&",
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split.map do |word|
+    if dictionary.keys.include?(word.downcase)
+      new_word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+  if tweet.map do |sentence|
+  puts word_substituter(sentence)
+  end
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet.split(" ").map do |new_sentence|
+    if new_sentence.length > 140
+      word_substituter(new_sentence)[0..140] + "..."
+    else
+      new_sentence
+  end.join(" ")
+end
